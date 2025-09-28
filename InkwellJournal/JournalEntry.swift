1981b1b03a87c3ebc9cdf04c412b56a692b1cd8d
Michael Fluharty <michael.fluharty@mac.com>
2025-09-28 15:27:03 -0500

    2025 SEP 28 Roll back all project files are in working state again

--- InkwellJournal/JournalEntry.swift
+++ InkwellJournal/JournalEntry.swift
@@ -1,6 +1,5 @@
 import SwiftData
 import Foundation
-import UIKit
 
 @Model
 class JournalEntry {
@@ -8,25 +7,18 @@ class JournalEntry {
     var content: String = ""
     var dateCreated: Date = Date()
     var mood: String = "Neutral"
-    var imageData: Data? = nil
     
-    init(title: String = "", content: String = "", mood: String = "Neutral", imageData: Data? = nil) {
+    init(title: String = "", content: String = "", mood: String = "Neutral") {
         self.title = title
         self.content = content
         self.dateCreated = Date()
         self.mood = mood
-        self.imageData = imageData
     }
     
     var formattedDate: String {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         return formatter.string(from: dateCreated)
-    }
-    
-    var image: UIImage? {
-        guard let imageData = imageData else { return nil }
-        return UIImage(data: imageData)
     }
 }
 
